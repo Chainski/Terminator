621a03eede8d76cd5b2779954829182a03ed0696
CHA1NSK1 <96607632+Chainski@users.noreply.github.com>
2024-05-02 11:20:57 -0500

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,2 +1,4 @@
+:: https://github.com/Chainski/Terminator
+@echo off
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /unsafe /platform:x64 /preferreduilang:en-US /out:Terminator.exe /target:exe Terminator.cs
 pause
